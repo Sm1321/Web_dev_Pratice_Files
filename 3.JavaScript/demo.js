1036fbcf681665978678cdce5f7d4e76c7dd8dc6
Mohan <siddulamohan1321@gmail.com>
2024-09-23 15:22:00 +0530

    Added the Javascript Files

---
+++ 3.JavaScript/demo.js
@@ -0,0 +1,36 @@
+var a = 10;
+console.log("intial",a);
+
+
+a = 30;
+console.log("second",a);
+
+
+
+
+/*
+var can be create the multiple variables name
+
+
+*/
+var a = 50;
+var a = 40;
+
+console.log(a)
+
+
+/*let -> it will get erro if we use the same variable name */
+let b;
+console.log("intialization",b)
+
+b = 20
+b = 50
+console.log("end",b)
+
+/*
+var and let has drawback of  
+*/
+
+/*if you decalre the Const,definntely you should declare */
+const c = 1;
+console.log(c)
