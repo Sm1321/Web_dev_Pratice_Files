cf0167edc36a87a99c1ebecef2999f2a963d1578
Mohan <siddulamohan1321@gmail.com>
2024-09-23 15:27:43 +0530

    Modified the JS file

--- 3.JavaScript/demo.js
+++ 3.JavaScript/demo.js
@@ -28,9 +28,26 @@ b = 50
 console.log("end",b)
 
 /*
-var and let has drawback of  
+Drawbacks of var and let:
+You mentioned drawbacks:
+var can cause issues because of re-declaration and function scoping, 
+     leading to unexpected behavior in certain contexts.
+let solves some of these issues by using block scope,
+     but it can still be reassigned, which might not always be ideal when you want a value to remain constant (which is why const exists).
 */
 
 /*if you decalre the Const,definntely you should declare */
 const c = 1;
 console.log(c)
+
+
+
+/*
+Key Points:
+var:-
+---->allows re-declaration and is function-scoped.
+let:-
+---->does not allow re-declaration in the same scope and is block-scoped.
+const:-
+----> must be initialized at the time of declaration and cannot be reassigned.
+*/
